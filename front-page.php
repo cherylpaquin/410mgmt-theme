22e092626307bfbf60cdba414ff53f07433252fa
cherylpaquin <111398531+cherylpaquin@users.noreply.github.com>
2022-08-31 15:23:25 -0400

    More Updates

--- front-page.php
+++ front-page.php
@@ -18,61 +18,65 @@ $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 
 ?>
 
 <div id="content" class="container-fluid">
-  <div id="hero" class="hero-banner row align-items-center justify-content-center" style="background: url('<?php echo $backgroundImg[0]
+  <div id="hero" class="hero-banner d-flex flex-column justify-content-center" style="background: url('<?php echo $backgroundImg[0]
 ; ?>') no-repeat;">
-    <div class="col-sm-8">
-      <header class="main-header d-flex flex-column align-items-center">
-          <?php the_post(); ?>
-          <!-- Title -->
-            <h1 class="display-1">
-              <a href="" class="typewrite" data-period="2000" data-type='[ "410", "Tour", "Production", "Travel", "Project" ]'>
-                <span class="wrap"></span>
-              </a>MGMT
-            </h1>
-            <div style="width: 30rem; height: 1px; background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 75%, rgba(255,255,255,0) 100%);" class="my-2" ></div>
-          <!-- Subtitle -->
-          <p class="lead"><?php bloginfo('description'); ?></p>
-      </header><!-- .main-header -->
-      <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-1b93190375e9ccc259df3a57c1abc0e64599724ae30d7ea4c6877eb615f89387.js"></script>
-      <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-      <script id="rendered-js">
-        (function () {
-          "use strict";
+    <div class="container-xxl">
+      <div class="row justify-content-center">
+        <div class="col-sm-8">
+          <header class="main-header d-flex flex-column align-items-center">
+              <?php the_post(); ?>
+              <!-- Title -->
+                <h1 class="display-1">
+                  <a href="" class="typewrite" data-period="2000" data-type='[ "410", "Tour", "Production", "Travel", "Project" ]'>
+                    <span class="wrap"></span>
+                  </a>MGMT
+                </h1>
+                <div style="width: 80%; height: 1px; background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 75%, rgba(255,255,255,0) 100%);" class="my-2" ></div>
+              <!-- Subtitle -->
+              <p class="lead"><?php bloginfo('description'); ?></p>
+          </header><!-- .main-header -->
+          <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-1b93190375e9ccc259df3a57c1abc0e64599724ae30d7ea4c6877eb615f89387.js"></script>
+          <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+          <script id="rendered-js">
+            (function () {
+              "use strict";
 
-          var carousels = function () {
-            $(".owl-carousel1").owlCarousel({
-              loop: true,
-              center: true,
-              margin: 0,
-              responsiveClass: true,
-              nav: false,
-              responsive: {
-                0: {
-                  items: 1,
-                  nav: false },
-
-                680: {
-                  items: 2,
+              var carousels = function () {
+                $(".owl-carousel1").owlCarousel({
+                  loop: true,
+                  center: true,
+                  margin: 0,
+                  responsiveClass: true,
                   nav: false,
-                  loop: false },
+                  responsive: {
+                    0: {
+                      items: 1,
+                      nav: false },
 
-                1000: {
-                  items: 3,
-                  nav: true } } });
+                    680: {
+                      items: 2,
+                      nav: false,
+                      loop: false },
+
+                    1000: {
+                      items: 3,
+                      nav: true } } });
 
 
 
-          };
+              };
 
-          (function ($) {
-            carousels();
-          })(jQuery);
-        })();
-        //# sourceURL=pen.js
-        </script><!-- #rendered-js -->
-      </div><!-- .col-sm-8 -->
-    </div><!-- #hero -->
+              (function ($) {
+                carousels();
+              })(jQuery);
+            })();
+            //# sourceURL=pen.js
+            </script><!-- #rendered-js -->
+          </div><!-- .col-sm-8 -->
+        </div><!-- .row -->
+      </div>
+    </div>
   <div id="feat-artists">
     <!-- Featured Artists Section -->
     <?php get_template_part('template-parts/featuredartists') ?>
