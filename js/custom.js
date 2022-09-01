77e386f6db53eb7783d01af0599e0a7e0f700e51
Adrian <lukabaseball@hotmail.com>
2022-09-01 18:22:36 -0400

    using js for scroll and removing scroll-behavior smooth from :root in scss

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,84 @@
 jQuery(function ($) {
-
-    var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+  $('a').on('click', function (e) {
+    // get anchor tag hash
+    const hash = this.hash;
+    //check if hash is present and consider urls ending with # like the back to top button and home button link
+    const hasHash = !!hash || this.href?.endsWith('#');
+    // check if hash exists and if it is a link to an element on the page
+    if (hasHash && location.hostname === this.hostname) {
+      e.preventDefault();
+      // get navbar height
+      const navHeight = $('#nav-main').innerHeight();
+      // animate to the element
+      $('html, body').animate(
+        {
+          scrollTop: (hash ? $(hash).offset().top : 0) - navHeight,
+        },
+        800,
+        function () {
+          // add hash to url
+          history.replaceState(null, null, hash || '#');
         }
+      );
+    }
+  });
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
 
-        var that = this;
-        var delta = 200 - Math.random() * 100;
+  TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-        if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
 
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
+    var that = this;
+    var delta = 200 - Math.random() * 100;
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #02498E}";
-        document.body.appendChild(css);
-    };
+    if (this.isDeleting) {
+      delta /= 2;
+    }
 
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    css.innerHTML = '.typewrite > .wrap { border-right: 0.08em solid #02498E}';
+    document.body.appendChild(css);
+  };
 }); // jQuery End
