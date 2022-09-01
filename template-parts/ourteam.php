431d099b6c6748d38f3f77539c59ed1a0d74b133
Adrian <lukabaseball@hotmail.com>
2022-09-01 19:59:58 -0400

    responsive styling for mobile and tablet...feel free to undo any of these as you see fit

--- template-parts/ourteam.php
+++ template-parts/ourteam.php
@@ -8,22 +8,22 @@
 			<div id="cards" class="row col-md-12 row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mx-0 px-0">
 				<?php if (have_rows('team', 'option')): ?>
 					<?php while (have_rows('team', 'option')): the_row();?>
-							<div class="col">
-								<div class="card h-100 team-member">
-									<div class="row col-sm-12 justify-content-center align-items-center">
-										<div class="team-photo col-sm-5">
-											<div class="profile-pic" style="background-image: url('<?php the_sub_field('team_photo');?>'); ">
-											</div>
+						<div class="col">
+							<div class="card h-100 team-member">
+								<div class="row col-sm-12 justify-content-center align-items-center">
+									<div class="team-photo col-sm-5">
+										<div class="profile-pic" style="background-image: url('<?php the_sub_field('team_photo');?>'); ">
 										</div>
-										<div class="team-bio col-sm-7">
-											<div class="card-body team-info">
-												<p> <span class="lead upper"><?php the_sub_field('name');?></span><br>
-												<?php the_sub_field('job_title');?></p>
-											</div>
+									</div>
+									<div class="team-bio col-sm-auto">
+										<div class="card-body team-info">
+											<p class="text-center"> <span class="lead upper"><?php the_sub_field('name');?></span><br>
+											<?php the_sub_field('job_title');?></p>
 										</div>
 									</div>
 								</div>
 							</div>
+						</div>
 					<?php endwhile;?>
 				<?php endif;?>
 			</div>
