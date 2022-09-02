2a734d4ffb08d6742eaf76e510b4697f94e0a3ec
Adrian <lukabaseball@hotmail.com>
2022-09-01 21:06:31 -0400

    reorder enqueue of owl styles

--- functions.php
+++ functions.php
@@ -5,10 +5,10 @@ add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
 function bootscore_child_enqueue_styles()
 {
 
-    // style.css
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('owl-carousel', get_stylesheet_directory_uri() . '/css/owl.carousel.min.css');
     wp_enqueue_style('owl-theme', get_stylesheet_directory_uri() . '/css/owl.theme.default.min.css');
+    // style.css
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
     // Compiled main.css
     $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/main.css'));
