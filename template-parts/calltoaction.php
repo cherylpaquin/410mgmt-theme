aae32edc325a42746235f9e8ce328e2c6ce4043b
cherylpaquin <111398531+cherylpaquin@users.noreply.github.com>
2022-09-17 19:42:31 -0400

    More Changes
    Part 2

--- template-parts/calltoaction.php
+++ template-parts/calltoaction.php
@@ -2,11 +2,25 @@
 	<div class="container-xxl">
 		<div class="row justify-content-center align-items-center">
 			<div class="col-sm-6">
-				<h1 class="display-1">
-					<div class="carousel slide" data-bs-ride="carousel">
-						<div class="carousel-inner">
-					</div>MGMT
-                </h1>
+				<h1 class="display-1" style="display: inline-flex;">
+					<?php if( have_rows('intro_text', 'option') ): 
+					  $i = 1;
+					    echo '<div class="carousel slide carousel-fade" data-bs-touch="false" data-bs-ride="carousel">
+					      <div class="carousel-inner">';
+
+					        while( have_rows('intro_text', 'option') ): the_row(); ?>
+
+					          <div class="carousel-item <?php if($i == 1) echo 'active'; ?>">
+					            <span style="color: #02498E;"><?php the_sub_field('intro_title', 'option'); ?></span>
+					          </div>
+					          <?php $i++;
+
+					        endwhile;
+					        echo '</div>
+					    </div>';
+				  	endif; ?>
+				  	MGMT
+				</h1>
 			</div>
 			<div class="col-sm-6">
 				<h1><?php the_field('cta_title', 'option'); ?></h1>
