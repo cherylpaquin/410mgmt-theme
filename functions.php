a23c7ed0abe8a74e907e2302301f16768e56122f
Adrian <lukabaseball@hotmail.com>
2022-09-19 19:55:54 -0400

    remove owl carousel and swiper enqueues the bs-swiper plugin enqueues swiper for us and owl carousel was removed

--- functions.php
+++ functions.php
@@ -4,10 +4,6 @@
 add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
 function bootscore_child_enqueue_styles()
 {
-
-    wp_enqueue_style('owl-carousel', get_stylesheet_directory_uri() . '/css/owl.carousel.min.css');
-    wp_enqueue_style('owl-theme', get_stylesheet_directory_uri() . '/css/owl.theme.default.min.css');
-    wp_enqueue_style('swiper-style', get_stylesheet_directory_uri() . '/css/swiper-bundle.min.css');
     // style.css
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
@@ -15,8 +11,6 @@ function bootscore_child_enqueue_styles()
     $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/css/main.css'));
     wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
 
-    wp_enqueue_script('owl-carousel', get_stylesheet_directory_uri() . '/js/owl.carousel.min.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('swiper', get_stylesheet_directory_uri() . '/js/swiper-bundle.min.js', array(), '1.0.0', true);
     wp_enqueue_script('jquery-mousewheel', get_stylesheet_directory_uri() . '/js/jquery.mousewheel.min.js', array('jquery'), '1.0.0', true);
 
     // custom.js
